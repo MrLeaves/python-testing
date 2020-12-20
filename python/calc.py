e56ebae78b9e6f71294dc8e80d9cf8ecb1d82f69
MrLeaves <806907827@qq.com>
2020-12-20 14:35:55 +0800

    add testcase

---
+++ python/calc.py
@@ -0,0 +1,13 @@
+class Calc:
+    #加减乘除
+    def add(self, a: int,b: int):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
